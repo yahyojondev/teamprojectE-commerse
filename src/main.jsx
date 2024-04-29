f9556dd59ac1ec65b22e0898807ec7652cfab0c0
yahyojondev <nabijonovdeveloper@gmail.com>
2024-04-29 23:51:48 +0500

    hardwork

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./sass/main.scss";
 import { BrowserRouter } from "react-router-dom";
 import store from "./context/store";
 import { Provider } from "react-redux";
